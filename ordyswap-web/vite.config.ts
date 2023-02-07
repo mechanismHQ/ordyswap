e696103069d8235ecc2abd44980d9edad4c54c92
Hank Stoever <hstove@gmail.com>
2023-02-06 17:53:10 -0800

    fix: repo name

--- ordyswap-web/vite.config.ts
+++ ordyswap-web/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.CI ? "/ordyswap" : "/",
+  base: process.env.CI ? "/ordyswap/" : "/",
   plugins: [react()],
 });
